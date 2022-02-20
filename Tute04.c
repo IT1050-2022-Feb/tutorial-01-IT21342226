a2e3ee8f855de129d8a61a337ad0fbc6d191b8da
AHAMED A.S.S <99683554+IT21342226@users.noreply.github.com>
2022-02-20 20:19:55 +0000

    tute 4 coding completed

--- Tute04.c
+++ Tute04.c
@@ -9,9 +9,9 @@ int minimum(int a,int b);
 int maximum(int a,int b);
 int multiply(int no1,int no2);
 
-int main() {
+int main() { //begining of main fuction
   
-   int no1, no2;
+   int no1, no2;//declaring variables
    printf("Enter a value for no 1 : ");
    scanf("%d", &no1);
    printf("Enter a value for no 2 : ");
@@ -20,33 +20,39 @@ int main() {
    printf("%d ", maximum(no1, no2));
    printf("%d ", multiply(no1, no2));
    return 0;
-}
-int maximum(int a, int b)
-{
-  if (a > b)
-  {
-    return a;
-  }
-  else if (b>a)
-  {
-    return b;
-  }
+}//ending of main fuction
 
-}
-
-int minimum(int a, int b)
+int minimum(int a, int b)//begining of minmum fuction
 {
+  int result2;
   if (a < b)
   {
-     return a;
+     result2 = a;
   }
     else if (b<a)
   {
-    return b;
+    result2 = b;
   }
+  return result2;
+  }//end of minimum fuction
 
-}
-int multiply(int no1, int no2)
+
+int maximum(int a, int b) //begining of maximum fuction
+{
+  int result1;
+  if (a > b)
+  {
+    result1 = a;
+  }
+  else if (b>a)
+  {
+    result1 = b;
+  }
+  return result1;
+
+}//end of maximum function
+  
+int multiply(int no1,int no2) //begin of multiply fuction
 {
    return no1 * no2 ;
-}
+}//end of multiply fuction
